034c2d528578dfcd25dc35654b753cf3de1e9ced
Fanuramal <faisalnuramal@gmail.com>
2021-03-23 21:37:12 +0700

    menambahkan folder assets

---
+++ assets/custom_js/debt.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	console.log("Sukses load debt.js!");
+	load_list_debt();
+});
+
+$('button#save_debt').on('click', function() {
+	$.ajax({
+		url: 'http://cashtory.rf.gd/cashtory/debt/add',
+		type: 'POST',
+		data: {
+			name: $('input[name="name"]').val(),
+			email: $('input[name="email"]').val(),
+			age: $('input[name="age"]').val()
+		},
+		dataType: 'json',
+		success: function(result)
+		{
+			console.log(result);
+			$('input[name="name"], input[name="email"], input[name="age"]').val('');
+			load_list_debt();
+		},
+		error: function(result)
+		{
+			console.log(result);
+		}
+	});
+});
+
+function load_list_debt()
+{
+	$.get('http://cashtory.rf.gd/cashtory/debt/get_list', function(result) {
+		console.log(result);
+		let new_list = '';
+		if (result.length > 0) 
+		{
+			result.forEach(data => {
+				new_list += `<li>${data.name}</li>`;
+			});
+		}
+		$('ul#list_name').html(new_list);
+	}, 'json');
+}
